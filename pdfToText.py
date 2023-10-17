c534cadfa02b8104e9aaa5cc779f2ab3a31d6b0e
Kelian <kelian.esteve@alumni.univ-avignon.fr>
2023-10-17 18:49:36 +0200

    Ajout du timer

--- pdfToText.py
+++ pdfToText.py
@@ -1,31 +1,40 @@
 import os
 import subprocess
 import sys
+import time
 
-def convert_pdfs_to_text(input_folder, output_folder):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
+def convert_pdfs_to_text(input_folder):
     for pdf_file in os.listdir(input_folder):
         if pdf_file.endswith(".pdf"):
             pdf_path = os.path.join(input_folder, pdf_file)
             txt_file = os.path.splitext(pdf_file)[0] + ".txt"
-            txt_path = os.path.join(output_folder, txt_file)
+            txt_path = os.path.join(input_folder, "text_files", txt_file)
 
-            # Utilisation de pdftotext pour convertir le PDF en texte
+            # Crée un sous-dossier "text_files" dans le dossier d'entrée si nécessaire
+            text_files_folder = os.path.join(input_folder, "text_files")
+            if not os.path.exists(text_files_folder):
+                os.makedirs(text_files_folder)
+
+            # Utilisez pdftotext pour convertir le PDF en texte
             subprocess.call(["pdftotext", pdf_path, txt_path])
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python pdf_to_text.py input_folder output_folder")
+    if len(sys.argv) != 2:
+        print("Usage: python pdf_to_text.py input_folder")
         sys.exit(1)
 
     input_folder = sys.argv[1]
-    output_folder = sys.argv[2]
 
     if not os.path.exists(input_folder):
         print(f"Le dossier d'entrée '{input_folder}' n'existe pas.")
         sys.exit(1)
 
-    convert_pdfs_to_text(input_folder, output_folder)
-    print("Conversion terminée.")
+    start_time = time.time()
+
+    convert_pdfs_to_text(input_folder)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Conversion terminée en {elapsed_time:.2f} secondes.")
+
+
